bc5363b80012ccf52969e6d902b4a69cf8b745fd
Carlos Rodrigues Paiva - PEQ <paiva@peq.coppe.ufrj.br>
2024-06-09 14:37:10 -0300

    Update para obter mapas

--- my_third_app.py
+++ my_third_app.py
@@ -8,13 +8,18 @@ import pandas as pd
 #    columns=('col %d' % i for i in range(20))
 #)
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
-
-st.line_chart(chart_data)
+#chart_data = pd.DataFrame(
+#     np.random.randn(20, 3),
+#     columns=['a', 'b', 'c'])
+#
+#st.line_chart(chart_data)
 
 #st.table(dataframe)
 
 #st.dataframe(dataframe.style.highlight_max(axis=0))
 
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
