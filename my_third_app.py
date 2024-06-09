957e90417d36effcb9412998144ab7498b63c183
Carlos Rodrigues Paiva - PEQ <paiva@peq.coppe.ufrj.br>
2024-06-09 15:58:28 -0300

    Updata caixa de seleção

--- my_third_app.py
+++ my_third_app.py
@@ -24,10 +24,19 @@ import pandas as pd
 
 #st.map(map_data)
 
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
+#x = st.slider('x')  # 👈 this is a widget
+#st.write(x, 'squared is', x * x)
+#
+#st.text_input("Your name", key="name")
+#
+## You can access the value at any point with:
+#st.session_state.name
 
-st.text_input("Your name", key="name")
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
 
-# You can access the value at any point with:
-st.session_state.name
+    chart_data
+
+    
