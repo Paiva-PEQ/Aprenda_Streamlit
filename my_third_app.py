6cf910e1fb24a0955f561a63e0b01a6a6fdcffc8
Carlos Rodrigues Paiva - PEQ <paiva@peq.coppe.ufrj.br>
2024-06-09 16:27:52 -0300

    import time

--- my_third_app.py
+++ my_third_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import time
 
 #dataframe = pd.DataFrame(
 #    np.random.randn(10, 20),
@@ -74,4 +75,16 @@ with right_column:
         ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
     st.write(f"You are in {chosen} house!")
 
-    
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'    
