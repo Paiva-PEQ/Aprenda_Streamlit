0125390754b4cc8c345ff042a3ecc01f80d55480
Carlos Rodrigues Paiva - PEQ <paiva@peq.coppe.ufrj.br>
2024-06-09 15:48:11 -0300

    update chave

--- my_third_app.py
+++ my_third_app.py
@@ -27,3 +27,7 @@ import pandas as pd
 x = st.slider('x')  # 👈 this is a widget
 st.write(x, 'squared is', x * x)
 
+st.text_input("Your name", key="name")
+
+# You can access the value at any point with:
+st.session_state.name
