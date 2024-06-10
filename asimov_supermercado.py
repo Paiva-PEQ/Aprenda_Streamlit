03ae0a446931f2115993edb4121268b00a94538b
Carlos Rodrigues Paiva - PEQ <paiva@peq.coppe.ufrj.br>
2024-06-10 17:17:28 -0300

    commit primeiro

---
+++ asimov_supermercado.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# Com uma visão mensal
+#faturamento por unidade… 
+# tipo de produto mais vendido, contribuição por filial,
+#Desempenho das forma de pagamento…
+#Como estão as avaliações das filiais?
+
+
+
+df = pd.read_csv("supermarket_sales.csv", sep=";", decimal=",")
+df
+
