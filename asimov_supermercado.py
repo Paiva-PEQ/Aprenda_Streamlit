bef30f99bbac29ef762e32c2764adb61cf6c963a
Carlos Rodrigues Paiva - PEQ <paiva@peq.coppe.ufrj.br>
2024-06-10 19:08:04 -0300

    plotly.express e wide

--- asimov_supermercado.py
+++ asimov_supermercado.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
-#import plotly.express as px
+import plotly.express as px
+
+st.set_page_config(layout="wide")
 
 # Com uma visão mensal
 #faturamento por unidade… 
