90a1bc8972bf168c268db517b5ab57edfaf07cec
Carlos Rodrigues Paiva - PEQ <paiva@peq.coppe.ufrj.br>
2024-06-10 19:42:35 -0300

    mês

--- asimov_supermercado.py
+++ asimov_supermercado.py
@@ -13,6 +13,9 @@ st.set_page_config(layout="wide")
 
 
 df = pd.read_csv("supermarket_sales.csv", sep=";", decimal=",")
-df["Date"] = pd.to_datetime(["Date"])
+df["Date"] = pd.to_datetime(df["Date"])
 df=df.sort_values("Date")
+
+df["Month"] = df["Date"].apply(lambda x: str(x.year) + "-" + str(x.month))
+month = st.sidebar.selectbox("Mês", df["Month"].unique())
 
