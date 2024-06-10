e5f9c47656c69ea9fdd898d5c36aee9e4cfb03c8
Carlos Rodrigues Paiva - PEQ <paiva@peq.coppe.ufrj.br>
2024-06-10 20:04:16 -0300

    update first grafico

--- asimov_supermercado.py
+++ asimov_supermercado.py
@@ -20,6 +20,9 @@ df["Month"] = df["Date"].apply(lambda x: str(x.year) + "-" + str(x.month))
 month = st.sidebar.selectbox("Mês", df["Month"].unique())
 
 df_filtered = df[df["Month"] == month]
-df_filtered
 
+col1, col2 = st.columns(2)
+col3, col4, col5 = st.columns(3)
 
+fig_date = px.bar(df_filtered, x="Date", y="Total", color="City", title="Faturamento por dia")
+col1.plotly_chart(fig_date, use_container_width=True)
